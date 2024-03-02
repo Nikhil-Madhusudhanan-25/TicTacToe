e9792a1fc7d34e7af626a8e3996498a423eb9e18
nikhil-madhusudhanan-25 <nikhilmadhusudhanan20@gmail.com>
2024-03-02 09:39:04 +0530

    Add function to check win condition

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameboard=(function(){
-    const board=[['X','O',''],['X','X','O'],['O','','X']];
+    const board=[['','',''],['','',''],['','','']];
     function returnBoard(){
         return board;
     }
@@ -18,6 +18,7 @@ const gameFlow=(function(){
     let board=gameboard.returnBoard();
     let row=0, column=0;
     function getMove(){
+        gameboard.displayBoard();
         console.log(`${currentActivePlayer.name}'s move - `);
         row=prompt("Enter row to mark");
         column=prompt("Enter column to mark");
@@ -32,8 +33,10 @@ const gameFlow=(function(){
             getMove();
         }
         gameboard.displayBoard();
-        switchPlayer();
-        getMove();
+        let result=checkWin();
+        if(result!=1)
+        {switchPlayer();
+        getMove();}
     }
     function switchPlayer(){
         if(currentActivePlayer==players.player1)
@@ -42,9 +45,27 @@ const gameFlow=(function(){
             currentActivePlayer=players.player1
         return currentActivePlayer;
     }
-    /* function test(){
-        console.log(currentActivePlayer.name);
+    function checkWin(){
+        let flattenedBoard=board.flat().map((element)=>{
+            if(element=='X')
+                return 1;
+            else if(element=='O')
+                return -1;
+            else
+                return 0;
+        });
+        let a,b,c;
+        let winArray=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+        for(element of winArray){
+            [a,b,c]=element;
+            if(flattenedBoard[a]+flattenedBoard[b]+flattenedBoard[c]==3){
+                console.log(currentActivePlayer.name+" wins!");
+                return 1;
+            }
+        }
     }
-    return{test}; */
-    return{getMove, mark};
+    /* function test(){
+    } 
+    return{test,getMove};*/
+    return{getMove};
 })();
